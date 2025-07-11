9116ecedf62e244eb5ff9627f4d8731de48d1d99
Retro & Chill <35567462+retroandchill@users.noreply.github.com>
2025-07-11 18:08:07 -0400

    Added support for allow the user to ignore a directory in the ProjectGlue folder

---
+++ Script/ManagedTurnBasedExamples/StructWithManagedProperty.cs
@@ -0,0 +1,17 @@
+﻿using System.Numerics;
+using UnrealSharp.Attributes;
+
+namespace ManagedTurnBasedExamples;
+
+[UStruct]
+public struct FStructWithManagedProperty
+{
+    [UProperty]
+    public string NativeProperty;
+
+    public int UnmarkedProperty;
+
+    public ILookup<int, string> ManagedProperty;
+
+    public Vector3 UnmanagedStruct;
+}
---
+++ Script/ProjectGlue/Extensions/TurnBasedCore/UnitAttribute.cs
@@ -0,0 +1,28 @@
+﻿namespace UnrealSharp.TurnBasedCore;
+
+public partial struct FBoolUnitAttribute
+{
+    public FBoolUnitAttribute(bool value)
+    {
+        BaseValue = value;
+        CurrentValue = value;
+    }
+}
+
+public partial struct FIntUnitAttribute
+{
+    public FIntUnitAttribute(int value)
+    {
+        BaseValue = value;
+        CurrentValue = value;
+    }
+}
+
+public partial struct FFloatUnitAttribute
+{
+    public FFloatUnitAttribute(float value)
+    {
+        BaseValue = value;
+        CurrentValue = value;
+    }
+}
